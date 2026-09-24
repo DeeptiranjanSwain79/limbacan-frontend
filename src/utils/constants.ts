9c850e858e444aa52522a48e7beb004c312ac192
Deeptiranjana Swain <deeptiranjanswain777@gmail.com>
2026-09-24 21:30:31 +0530

    constants updated

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,10 +4,10 @@ export const BASE_URL_LOCAL = "http://localhost:5000/api/v1";
 
 export const BASE_URL_ONLY_LOCAL = "http://localhost:5000";
 
-// export const BASE_URL = "https://plantarscan-backend.onrender.com/api/v1";
+export const BASE_URL = "https://plantarscan-backend.onrender.com/api/v1";
 
-// export const BASE_URL_ONLY = "https://plantarscan-backend.onrender.com";
+export const BASE_URL_ONLY = "https://plantarscan-backend.onrender.com";
 
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
 
-export const BASE_URL_ONLY = "http://localhost:5000";
+// export const BASE_URL_ONLY = "http://localhost:5000";
